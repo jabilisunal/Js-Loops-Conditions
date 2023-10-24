4cb6f584ce08ae89ac406bc6cb89225b675fec4c
jabilisunal <jabilisunal@gmail.com>
2023-10-24 19:41:12 +0400

    inital commit

---
+++ script.js
@@ -0,0 +1,91 @@
+//  ? 2) 1-den 100 qeder cut ededlerin sayini tapan alqoritm yazın    js-de.
+// let count =0;
+// for (let i = 1; i < 100; i++) {
+//     if(i%2==0){
+//         count++;
+//     }    
+// }
+// console.log(count);
+
+// ? 3 iki ededin deyerlerini deyishen elave etmeden deyishin.
+
+// let a = 25;   //5
+// let b = 35;    //10
+
+// a = a + b;   // 5+10
+// console.log(a + "a deyeri")
+// b= a-b;   // 15-5
+// console.log(b + " b deyeri");
+// a= a-b;    //15-10 
+// // console.log(a)
+
+// console.log(a,b)
+
+
+// ? 4) Daxil olan ededin faktorialin tapan alqoritm yazin.
+
+// let num = parseInt(prompt("Eded daxil edin"));
+
+// let fact =1;
+
+// for (let i = 1; i <= num; i++) {
+
+//     fact *= i;
+// }
+// console.log(fact);
+
+//  ? 7)Array-de (minimum 5 element olsun) elementlerden menfi ededleri gosteren alqoritm yazin.
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//         console.log(arr[i])
+//     }
+// }
+
+// ? 8)Göndədrilmiş Arrayde cut ededler cemi ile tek ededlerin ceminin ferqini tapan alqoritm yazin.  tapan alqoritm.
+
+
+// const arr = [12, 43, 12, 43, 34];
+
+// let sumTek = 0;
+// let sumCut =0;
+
+// for (let i = 0; i < arr.length; i++) {
+//    if(arr[i]%2==0){
+//     sumCut+=arr[i];
+//    }
+//    else if(arr[i]%2!==0){
+//     sumTek+=arr[i]
+//    }
+// }
+// console.log(sumCut);
+// console.log(sumTek);
+
+
+//  ? 10) Verilmish Array-de cut indeksde duran ededleri cemin tapin .
+
+// console.log (sumCut-sumTek);
+// const arr=[77,75,85,95,90]
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i]%2===0) {  
+//         console.log(arr[i]);     
+//     }   
+// }
+
+//  ?  11)Göndərilmiş array-dəki ən böyük ədədi tapan  alqoritm yazin.
+
+// const arr = [77, 75, 85, 95, 90]
+// let num = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if (num < arr[i]) {
+//         num = arr[i]
+//     }
+// }
+
+
+//  console.log(num);
+
+
+
+
